3ac23b3409045579021c0a8934c75671f78308a5
Betul Sahin <betul.sahin@huawei.com>
2023-01-05 14:23:08 +0300

    Create 3 projects

---
+++ movie-info-service/src/main/java/com/betulsahin/javabrains/movieinfoservice/MovieInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.betulsahin.javabrains.movieinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServiceApplication.class, args);
+	}
+
+}
